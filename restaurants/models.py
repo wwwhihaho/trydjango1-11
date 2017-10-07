78350b7e8e80d91a15ab8f3a466a99083dfba782
wwwhihaho <wwwhihaho@gmail.com>
2017-10-07 14:16:52 +0900

    saving data ther hard + wrong way

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,10 +1,37 @@
 from django.db import models
+from django.db.models.signals import pre_save, post_save
 
-# Create your models here.
+from .utils import unique_slug_generator
+
+
 class RestaurantLocation(models.Model):
     name            = models.CharField(max_length=120) 
     location        = models.CharField(max_length=120, null=True, blank=True) 
     category        = models.CharField(max_length=120, null=True, blank=True)
     timestamp       = models.DateTimeField(auto_now_add=True)
     updated         = models.DateTimeField(auto_now=True)
+    slug            = models.SlugField(null=True, blank=True)
     #my_date_field   = models.DateField(auto_now=False, auto_now_add=False)
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def title(self):
+        return self.name #obj.title
+
+
+def rl_pre_save_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
+
+# def rl_post_save_receiver(sender, instance, created, *args, **kwargs):
+#     print('saved')
+#     print(instance.timestamp)
+#     if not instance.slug:
+#         instance.slug = unique_slug_generator(instance)
+#         instance.save() 
+
+pre_save.connect(rl_pre_save_receiver, sender=RestaurantLocation)
+
+# post_save.connect(rl_post_save_receiver, sender=RestaurantLocation)
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,28 +1,46 @@
-import random
+from django.db.models import Q
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
 
-# Create your views here.
-class HomeView(TemplateView):
-    template_name = 'home.html'
+from .models import RestaurantLocation
 
-    def get_context_data(self, *args, **kwargs):
-        context = super(HomeView, self).get_context_data(*args, **kwargs)
-        num = None
-        some_list = [
-            random.randint(0, 10000000), 
-            random.randint(0, 10000000), 
-            random.randint(0, 10000000)
-            ]
-        testlist = [1,2,3,4,5]
-        condition_bool_item = True
-        if condition_bool_item:
-            num = random.randint(0, 10000000)
-        context = {
-            "num" : num, 
-            "some_list" : some_list,
-            "testlist" : testlist
-        }
-        return context
+def restaurant_listview(request):
+    template_name = 'restaurants/restaurantlocation_list.html'
+    queryset = RestaurantLocation.objects.all()
+    context = {
+        "object_list" : queryset
+    }
+    return render(request, template_name, context)
+
+def restaurant_detailview(request, slug):
+    template_name = 'restaurants/restaurantlocation_detail.html'
+    obj = RestaurantLocation.objects.get(slug=slug)
+    context = {
+        "object" : obj
+    }
+    return render(request, template_name, context)
+
+
+class RestaurantListView(ListView):
+    template_name = 'restaurants/restaurantlocation_list.html'
+
+    def get_queryset(self):
+        slug = self.kwargs.get("slug")
+        if slug:
+            queryset = RestaurantLocation.objects.filter(
+                Q(category__iexact=slug) |
+                Q(category__icontains=slug)
+            )
+        else:
+            queryset = RestaurantLocation.objects.all()
+        return queryset
+
+class RestaurantDetailView(DetailView):
+    queryset = RestaurantLocation.objects.all()
+
+    # def get_object(self, *arg, **kwarg):
+    #     rest_id = self.kwargs.get('rest_id')
+    #     obj = get_object_or_404(RestaurantLocation, id=rest_id) # pk = rest_id
+    #     return obj
